7671193a8ddb5d5d37cb5641935883db4898d463
zury <zurydevs@gmail.com>
2024-09-06 21:58:10 -0400

    Change in style and contact form

--- src/components/emailForm.tsx
+++ src/components/emailForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 
 interface EmailData {
@@ -9,7 +9,7 @@ interface EmailData {
   message: string;
 }
 
-const EmailForm: React.FC = () => {
+const EmailForm: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
@@ -18,6 +18,22 @@ const EmailForm: React.FC = () => {
   const [isSending, setIsSending] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  const formRef = useRef<HTMLDivElement>(null);
+
+  // Cierra el formulario si se hace clic fuera de él
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (formRef.current && !formRef.current.contains(event.target as Node)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -41,7 +57,6 @@ const EmailForm: React.FC = () => {
       });
 
       if (response.ok) {
-        console.log("Email sent successfully");
         setShowSuccessMessage(true);
         setEmail("");
         setSubject("");
@@ -50,11 +65,9 @@ const EmailForm: React.FC = () => {
         setName("");
       } else {
         const errorData = await response.json();
-        console.error("Error sending email:", errorData);
         setShowErrorMessage(true);
       }
     } catch (error) {
-      console.error("Error sending email:", error);
       setShowErrorMessage(true);
     } finally {
       setIsSending(false);
@@ -64,90 +77,104 @@ const EmailForm: React.FC = () => {
   const containerVariants = {
     initial: { opacity: 0, y: -50 },
     animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-    exit: { opacity: 0, y: -50, transition: { duration: 0.3 } }
+    exit: { opacity: 0, y: -50, transition: { duration: 0.3 } },
   };
 
   return (
     <motion.div
-      className="bg-white p-6 rounded-xl shadow-md max-w-sm mx-auto mt-32 mb-10 w-80 z-50 fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center"
-      variants={containerVariants}
+      className="fixed inset-0 flex items-center justify-center z-[10000]" // Z-index alto para estar sobre todo
       initial="initial"
       animate={showSuccessMessage ? "exit" : "animate"}
+      variants={containerVariants}
     >
-      <form onSubmit={handleSubmit} className="space-y-2 mx-4">
-        <div>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            placeholder="Ingrese su Nombre"
-            className="w-full p-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-          />
-        </div>
-        <div>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            placeholder="Ingrese su Email"
-            className="w-full p-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-          />
-        </div>
-        <div>
-          <input
-            type="tel"
-            id="phone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            required
-            placeholder="Ingrese su N\u00b0 Telefonico"
-            className="w-full p-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="subject"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            required
-            placeholder="Asunto"
-            className="w-full p-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-          />
-        </div>
-        <div>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-            rows={4}
-            placeholder="Mensaje"
-            className="w-full p-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-          />
-        </div>
-
+      {/* Fondo oscuro para crear un efecto de superposición */}
+      <div className="fixed inset-0 bg-black bg-opacity-50" />
+      
+      <div ref={formRef} className="bg-white p-8 rounded-xl shadow-lg w-full max-w-[400px] mx-auto sm:w-[450px] z-[10001] relative">
+        {/* Botón de cerrado */}
         <button
-          type="submit"
-          className="bg-[#1B9AAA] hover:bg-[#27B9CB] text-white font-medium py-2 px-4 rounded-lg w-full transition duration-200 disabled:opacity-50"
-          disabled={isSending}
+          onClick={onClose}
+          className="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
         >
-          {isSending ? "Sending..." : "Send Email"}
+          ✖
         </button>
 
-        {showSuccessMessage && (
-          <p className="text-green-500 text-center">Email sent successfully!</p>
-        )}
-        {showErrorMessage && (
-          <p className="text-red-500 text-center">Error sending email. Please try again.</p>
-        )}
-      </form>
+        <form onSubmit={handleSubmit} className="space-y-4 w-full">
+          <div>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              placeholder="Nombre"
+              className="w-full p-4 border text-lg rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Email"
+              className="w-full p-4 border text-lg rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+            />
+          </div>
+          <div>
+            <input
+              type="tel"
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+              placeholder="Teléfono"
+              className="w-full p-4 border text-lg rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              id="subject"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+              required
+              placeholder="Asunto"
+              className="w-full p-4 border text-lg rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+            />
+          </div>
+          <div>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+              rows={5}
+              placeholder="Mensaje"
+              className="w-full p-4 border text-lg rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-3 px-4 rounded-lg w-full transition duration-200 disabled:opacity-50"
+            disabled={isSending}
+          >
+            {isSending ? "Enviando..." : "Enviar"}
+          </button>
+
+          {showSuccessMessage && (
+            <p className="text-green-500 text-center">¡Email enviado con éxito!</p>
+          )}
+          {showErrorMessage && (
+            <p className="text-red-500 text-center">Error al enviar el email. Intente de nuevo.</p>
+          )}
+        </form>
+      </div>
     </motion.div>
   );
 };
 
 export default EmailForm;
+
--- src/components/floating-navbar.tsx
+++ src/components/floating-navbar.tsx
@@ -1,40 +1,36 @@
 "use client";
 import React from "react";
 import { FloatingNav } from "@/components/ui/floatingNav";
-import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 import { IoHomeOutline } from "react-icons/io5";
 import { FcAbout } from "react-icons/fc";
-import Link from "next/link";
+import { MdWorkOutline } from "react-icons/md";
+import { RiGalleryLine } from "react-icons/ri";
 
 export function FloatingNavBar() {
   const navItems = [
     {
       name: "Inicio",
       link: "/",
-      icon: <IoHomeOutline className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: <IoHomeOutline className="h-6 w-6 text-neutral-500 dark:text-white block lg:hidden" />, // Solo se muestra en móviles
     },
     {
       name: "Acerca de",
       link: "/about",
-      icon: <FcAbout className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: <FcAbout className="h-6 w-6 text-neutral-500 dark:text-white block lg:hidden" />, // Solo se muestra en móviles
     },
     {
       name: "Servicios",
       link: "/services",
-      icon: (
-        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-      ),
+      icon: <MdWorkOutline className="h-6 w-6 text-neutral-500 dark:text-white block lg:hidden" />, // Solo se muestra en móviles
     },
     {
-        name: "Multimedia",
-        link: "/multimedia",
-        icon: (
-          <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-        ),
-      },
+      name: "Multimedia",
+      link: "/multimedia",
+      icon: <RiGalleryLine className="h-6 w-6 text-neutral-500 dark:text-white block lg:hidden" />, // Solo se muestra en móviles
+    },
   ];
   return (
-    <div className="relative  w-full ">
+    <div className="relative w-full">
       <FloatingNav navItems={navItems} />
     </div>
   );
